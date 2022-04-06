03a86ebeef451cb7da5d970597b70b2bd0289b1b
inspirers <arvid.boisen@protonmail.com>
2022-04-06 17:22:08 +0200

    added function

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,34 @@
 use std::io;
 use std::io::Write;
 
-fn main() {
-    print!("Enter amount for person 1: ");
+fn input(n: u8) -> f32 {
+    print!("Enter amount for person {}: ", n);
+
     io::stdout().flush().unwrap();
-    let mut input_line1 = String::new();
+    let mut input_line = String::new();
     io::stdin() // the rough equivalent of `std::cin`
-        .read_line(&mut input_line1) // actually read the line
+        .read_line(&mut input_line) // actually read the line
         .expect("Failed to read line"); // which can fail, however
-    let x: f32 = input_line1
+
+    let x: f32 = input_line
         .trim() // ignore whitespace around input
         .parse() // convert to integers
-        .expect("Input not an integer"); // which, again, can fail
+        .expect("Failed to parse from string to float");
+    return x;
+}
 
-    print!("Enter amount for person 2: ");
-    io::stdout().flush().unwrap();
-    let mut input_line2 = String::new();
-    io::stdin() // the rough equivalent of `std::cin`
-        .read_line(&mut input_line2) // actually read the line
-        .expect("Failed to read line"); // which can fail, however
-    let y: f32 = input_line2
-        .trim() // ignore whitespace around input
-        .parse() // convert to integers
-        .expect("Input not an integer"); // which, again, can fail
-
+fn calc(x: f32, y: f32) {
     if x > y {
-        println!("Person 2 should pay Person 1: {}", ((x - y)/2.0));
-
-    }
-    else if y > x {
-        println!("Person 1 should pay Person 2: {}", ((y - x)/2.0));
-    }
-    else if y == x {
+        println!("Person 2 should pay person 1: {}", ((x - y) / 2.0));
+    } else if y > x {
+        println!("Person 1 should pay person 2: {}", ((y - x) / 2.0));
+    } else if y == x {
         println!("No transfer should be made.");
     }
-        //println!("{} {}", x, y);
+}
+
+fn main() {
+    let x: f32 = input(1);
+    let y: f32 = input(2);
+    calc(x, y);
 }
